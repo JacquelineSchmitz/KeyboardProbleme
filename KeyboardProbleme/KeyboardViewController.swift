edb63506cfafcd7a01d65ea3d3a517e9e786f1be
jacquelineschmitz <jacquelineschmitz81@gmail.com>
2022-11-23 15:35:13 +0100

    ready

--- KeyboardProbleme/KeyboardViewController.swift
+++ KeyboardProbleme/KeyboardViewController.swift
@@ -7,23 +7,50 @@
 
 import UIKit
 
-class KeyboardViewController: UIViewController, UITextFieldDelegate {
+class KeyboardViewController: UIViewController, UITextFieldDelegate, UITextViewDelegate {
     
-
+    
     
     @IBOutlet weak var erstesTextField: UITextField!
+    @IBOutlet weak var textView: UITextView!
     @IBOutlet weak var zweitesTextField: UITextField!
-    
+    @IBOutlet weak var drittesTextField: UITextField!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        
         erstesTextField.delegate = self
+        textView.delegate = self
         zweitesTextField.delegate = self
+        drittesTextField.delegate = self
+        textView.inputAccessoryView = createToolbar()
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+        
+        
         self.dismissKeyboard()
-       
+        
+        
     }
-
+    
+    @objc func keyboardWillShow(notification: NSNotification){
+        if self.view.frame.origin.y == 0 && (drittesTextField.isFirstResponder) {
+            if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+                self.view.frame.origin.y -= keyboardSize.height
+            }
+        }
+    }
+    
+    @objc func keyboardWillHide(notificayion: NSNotification) {
+        if self.view.frame.origin.y != 0 {
+            self.view.frame.origin.y = 0
+        }
+    }
+    
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         view.endEditing(true)
         return true
@@ -39,5 +66,30 @@ class KeyboardViewController: UIViewController, UITextFieldDelegate {
         view.addGestureRecognizer(tap)
     }
     
-}
+    @objc func donePressed() {
+        textView.endEditing(true)
+    }
+    
+    @objc func trashPressed() {
+        textView.endEditing(true)
+    }
+    
+        func createToolbar() -> UIToolbar{
+            let toolbar = UIToolbar()
+            toolbar.sizeToFit()
+            
+            let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(donePressed))
+            let trashButton = UIBarButtonItem(barButtonSystemItem: .trash, target: nil, action: #selector(trashPressed))
+            
+            toolbar.setItems([doneButton, trashButton], animated: true)
+            return toolbar
+            
+            
+            
+            //            toolbar.setItems([trashButton], animated: true)
+            
+        }
+        
+        
+    }
 
